1d76dd7f43db071fb64bfa29915ac7db339905af
leoneves <eleonardoneves@gmail.com>
2024-05-14 22:36:03 -0300

    split logic to create or update assessments

--- app/projects/project_management.rb
+++ app/projects/project_management.rb
@@ -11,16 +11,13 @@ module ProjectManagement
       project = Project.find_by(id: params[:id])
 
       if project.nil?
-        project = Project.new(name: params[:name])
-        project.save!
+        project = create_new_project(params)
       else
-        project.update(name: params[:name])
+        project = update_project(project, params)
         brand_new = false
       end
 
-      create_assessment(params[:assessments], project.id)
-
-      response(success: project.persisted?, value: project, brand_new: brand_new)
+      response(success: true, value: project, brand_new: brand_new)
     rescue ActiveRecord::RecordInvalid
       raise ActiveRecord::Rollback
     end
@@ -36,11 +33,35 @@ module ProjectManagement
     assessments.each do |assessment|
       next if assessment[:grades].empty?
 
-      persisted_assessment = Assessment.create(project_id: project_id)
-      assessment[:grades]&.each do |grade|
+      persisted_assessment = Assessment.new(project_id: project_id)
+
+      grades = assessment[:grades]&.map do |grade|
         criteria = Criteria.create(weight: grade[:criteria][:weight])
-        Grade.create(grade: grade[:grade], assessment_id: persisted_assessment.id, criteria_id: criteria.id)
+        Grade.new(grade: grade[:grade], assessment_id: persisted_assessment.id, criteria_id: criteria.id)
       end
+      persisted_assessment.grades = grades
+      persisted_assessment.weighted_mean = Assessments.calc_weighted_mean(persisted_assessment)
+      persisted_assessment.save!
     end
   end
+
+  def create_new_project(params)
+    project = Project.new(name: params[:name])
+    project.save!
+    create_assessment(params[:assessments], project.id)
+    project.update!(average: Assessments.calc_average(project.assessments)) if project.assessments.present?
+    project
+  end
+
+  # I will implement later
+  def update_assessment(_assessments, _project); end
+
+  def update_project(project, params)
+    project.update(name: params[:name])
+    update_assessment(params[:assessments], project.id)
+    project.update!(average: Assessments.calc_average(project.assessments)) if project.assessments.present?
+    project
+  end
+
+  private_methods %i[create_new_project update_project]
 end
--- spec/projects/project_management_spec.rb
+++ spec/projects/project_management_spec.rb
@@ -29,6 +29,56 @@ RSpec.describe ProjectManagement do
       it { is_expected.not_to be_brand_new }
     end
 
+    context 'with assessments' do
+      let(:params) do
+        {
+          name: 'p1',
+          assessments: [
+            {
+              grades: [
+                {
+                  grade: 1,
+                  criteria: {
+                    weight: 2
+                  }
+                },
+                {
+                  grade: 2,
+                  criteria: {
+                    weight: 1
+                  }
+                }
+              ]
+            },
+            {
+              grades: [
+                {
+                  grade: 4,
+                  criteria: {
+                    weight: 2
+                  }
+                },
+                {
+                  grade: 8,
+                  criteria: {
+                    weight: 1
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      end
+
+      it { is_expected.to be_success }
+
+      it do
+        create_or_update_project
+        project = Project.find_by(name: 'p1')
+        expect(project.average).to eq(3.33)
+      end
+    end
+
     context 'with error while save project' do
       before { allow_any_instance_of(Project).to receive(:save!).and_raise(ActiveRecord::RecordInvalid) }
 
@@ -36,7 +86,7 @@ RSpec.describe ProjectManagement do
     end
 
     context 'with error while save assessments' do
-      before { allow(Assessment).to receive(:create).and_raise(ActiveRecord::RecordInvalid) }
+      before { allow_any_instance_of(Assessment).to receive(:save!).and_raise(ActiveRecord::RecordInvalid) }
 
       let(:params) do
         {
@@ -44,7 +94,10 @@ RSpec.describe ProjectManagement do
           assessments: [
             grades: [
               {
-                grade: 7.5
+                grade: 7.5,
+                criteria: {
+                  weight: 2
+                }
               }
             ]
           ]
